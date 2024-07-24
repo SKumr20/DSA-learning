83e4c2130ecfc96091cd41d128aa72ea4eb9862e
Satyam Kumar <113851151+SKumr20@users.noreply.github.com>
2024-07-24 22:19:43 +0530

    modified:   pattern1.cpp

---
+++ Patterns/pattern1.cpp
@@ -0,0 +1,24 @@
+/*  Pattern - 
+    *****
+    *****
+    *****
+    *****
+    *****
+*/
+
+
+#include <iostream>
+
+int main()
+{
+    int N;
+    std::cin>>N;
+    for (int i=0; i<N; i++)
+    {
+        for (int j=0; j<N; j++)
+        {
+            std::cout<<"* ";
+        }
+        std::cout<<" "<<std::endl;
+    }
+}
