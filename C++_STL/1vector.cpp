4e30ce7509f8a9715d16c204f8bee2cdf1de523a
Satyam Kumar <113851151+SKumr20@users.noreply.github.com>
2024-08-01 21:39:15 +0530

    modified:   C++_STL/1vector.cpp

--- C++_STL/1vector.cpp
+++ C++_STL/1vector.cpp
@@ -56,8 +56,48 @@ void explainVector() {
     // Shortcut
 
     // auto = automatically assigns a vector iterator, takes care of data types of vector
+    // This is a for-each loop
     for (auto it : v) {
         cout << it << " ";
     }
+
+    // Erase elements from vector
+    // {10, 20, 12, 23}
+    v.erase(v.begin()+1); // 20 gets erased
+
+    // Erase multiple elements  - {Start, end} parameter
+    // {10, 20, 12, 23, 35}
+    v.erase(v.begin() + 2, v.begin() + 4); // {10, 20, 35}
+
+    // Insert function
+
+    vector<int>v{2, 100}; // Creates {100, 100}
+    v.insert(v.begin(), 300); // {300, 100, 100}
+
+    // Insert many elements
+    v.insert(v.begin() + 1, 2, 10); // {300, 10, 10, 100, 100} Element 10 gets inserted 2 times
+
+    // Insert a vector into another vector
+    vector<int> copy(2, 50); // Vector named copy 
+
+    // Original v vector from above = {300, 10, 10, 100, 100}
+    // Add vector copy to vector v, from begin of copy to end of copy
+    v.insert(v.begin(), copy.begin(), copy.end()); // {50, 50, 300, 10, 10, 100, 100}
+
+    // Get size of a vector - number of elements inside a vector
+    cout << v.size(); // 2
+
+    // Pop_back - removes the last element
+    // v = {10, 20}
+    v.pop_back(); // {10}
+
+    // v1 -> {10, 20}
+    // v2 -> {30, 40}
+    v1.swap(v2); // v1 -> {30, 40}, v2 -> {10, 20}
+
+    v.clear(); // erases entire vector
+
+    // True if vector is empty
+    cout << v.empty();
 
 };
